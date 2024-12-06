eda97486b45fe3fe2ff09c4fbbdcd64388a4fd66
francisco <mourajuniorf@gmail.com>
2024-12-06 18:49:44 -0300

    atualização de layout para o carteira

--- front-end/src/app/pages/carteira/carteira.component.ts
+++ front-end/src/app/pages/carteira/carteira.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Carteira } from 'src/app/models/carteira.model';
 import { CarteiraService } from 'src/app/services/carteira.service';
+import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-carteira',
@@ -16,6 +18,10 @@ export class CarteiraComponent implements OnInit {
   mostrarModal: boolean = false;
   metodoSelecionado: string | null = null;
   isLoading: boolean = false; // Estado de loading
+  nomeLoja: string = "Easy-Parking"; // Nome da loja
+  cidade: string = "Barueri"; // Nome da cidade
+
+  chavePix = '05121324456';
 
   dadosCartao = {
     numero: '',
@@ -27,9 +33,12 @@ export class CarteiraComponent implements OnInit {
   qrCodeSrc: string = '';
   codigoPix: string = '';
 
-  constructor(private carteiraService: CarteiraService, private http: HttpClient) { }
+  constructor(private carteiraService: CarteiraService, private http: HttpClient, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
+    if (this.metodoSelecionado === 'pix') {
+      this.gerarQRCodePIX();
+    }
     this.carteira = this.carteiraService.obterCarteira();
   }
 
@@ -52,21 +61,21 @@ export class CarteiraComponent implements OnInit {
 
   
   gerarQRCodePIX() {
-    const valor = this.valorOperacao;
-    this.isLoading = true; // Inicia o carregamento enquanto gera o QR Code
+    const payload = this.montarPayloadPix();  // Monta o payload do PIX
+    QRCode.toDataURL(payload, { width: 300, margin: 1 }, (err, url) => {  // Gera o QR Code
+      if (err) {
+        console.error('Erro ao gerar QR Code', err);
+      } else {
+        this.qrCodeSrc = url;  // Define a URL do QR Code gerado
+        this.codigoPix = payload; // Define o código PIX gerado
+      }
+    });
+  }
 
-    // Enviar a requisição para gerar o QR Code
-    this.http.post('http://localhost:8080/api/pix', { valor: valor })
-      .subscribe((response: any) => {
-        this.codigoPix = response.codigoPix;
-        this.qrCodeSrc = `data:image/png;base64,${response.qrCodeBase64}`;
 
-        // Depois de gerar o QR Code, aguarde a confirmação do pagamento
-        this.simularPagamento();
-      }, error => {
-        console.error('Erro ao gerar o QR Code:', error);
-        this.isLoading = false; // Desabilita o loading em caso de erro
-      });
+  montarPayloadPix(): string {
+    const valorFormatado = (this.valorOperacao * 100).toString(); // Valor em centavos
+    return `00020101021126580014BR.GOV.BCB.PIX0114${this.chavePix}520400005303986540${valorFormatado}5802BR5908${this.nomeLoja}6009${this.cidade}62070503***6304`;
   }
 
   // Função para simular o processamento do pagamento
