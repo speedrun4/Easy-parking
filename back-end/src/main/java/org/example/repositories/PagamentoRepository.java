d67b90f1b2c7f28e7e2e66d2a396bfbc1cd192ad
Francisco Moura <mourajuniorf@gmail.com>
2025-07-22 16:54:10 -0300

    ajuste no liste

--- back-end/src/main/java/org/example/repositories/PagamentoRepository.java
+++ back-end/src/main/java/org/example/repositories/PagamentoRepository.java
@@ -4,6 +4,8 @@ import org.example.models.Pagamentos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PagamentoRepository extends JpaRepository<Pagamentos, Long> {
     List<Pagamentos> findByUsuarioId(Integer usuarioId);
